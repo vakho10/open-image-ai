4d9f9ed4dbc65ff0def57d8b83faebc31c4a3f67
Vakho <vakho10@gmail.com>
2022-07-24 16:51:22 +0400

    Add Anime16K support

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ ImgLarger class uses [imglarger](https://imglarger.com/) API to increase the siz
 ## Cartoonize
 Cartoonize class uses [cartoonize](https://cartoonize-lkqov62dia-de.a.run.app/cartoonize) API to turn images into cartoon ones. Pretty fun to use 
 and to be frank the results are quite good too.
+
+## Anime16K
+Anime16K class uses [anime16k](https://imglarger.com/Anime16K) API to increase the size of a cartoon image (2 times, 4 times or even 8 times).
---
+++ src/main/java/ge/vakho/ai/anime16k/Anime16KDemo.java
@@ -0,0 +1,49 @@
+package ge.vakho.ai.anime16k;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+public class Anime16KDemo {
+
+    public static void main(String[] args) throws IOException {
+        Path srcPath = Paths.get("F:/data_src");
+        Path dstPath = srcPath.resolveSibling("data_dst");
+
+        // Create the output folder if it doesn't exist
+        if (Files.notExists(dstPath)) {
+            Files.createDirectories(dstPath);
+        }
+
+        // I use executor service to launch multiple processes at once
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        List<String> processedFiles = Files.walk(dstPath)
+                .filter(Files::isRegularFile)
+                .map(Path::getFileName)
+                .map(Path::toString)
+                .map(filename -> filename.substring(0, filename.lastIndexOf(".")))
+                .collect(Collectors.toList());
+
+        Files.walk(srcPath)
+                // (1) Grab all images from folder
+                .filter(Files::isRegularFile)
+                // ! Ignore previously processes files !
+                .filter(file -> !processedFiles.contains(file.getFileName().toString().substring(0, file.getFileName().toString().lastIndexOf("."))))
+                // (2) Wrap in background remover class
+                .map(inputImage -> {
+                    final String filename = inputImage.getFileName().toString();
+                    return new Anime16K(
+                            inputImage,
+                            dstPath.resolve(filename.substring(0, filename.lastIndexOf(".")).concat(".jpg")));
+                })
+                // (3) Execute tasks
+                .forEach(executorService::submit);
+    }
+
+}
